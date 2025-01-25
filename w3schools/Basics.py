a01d3d0b156aa1aaee4bbc503bf36791d9147515
Noman Ahmed <NomanAhmed@macbookpro.mynetworksettings.com>
2025-01-25 16:45:00 -0500

    updates

---
+++ w3schools/Basics.py
@@ -0,0 +1,31 @@
+import sys
+
+print("Hello, World!")
+print(sys.version)
+
+f = 2
+i = 10
+s = "test"
+
+print(f+f)
+print(i+i)
+print(f+i)
+print(s+s)
+
+a = 10
+b = 2.3
+c = a*b
+print(type(c))
+
+new_list = ["a", "b", "c", "d"]
+print(new_list[-2])
+
+alphabets = {'1':'A','2':'B','3': ['C','D','E']}
+print(alphabets['3'][2])
+
+list = ["Great", 51, True, 46, "Hi"]
+print(list[2:-1])
+
+list_mul = ['2','2','2']
+print(list_mul*2)
+
