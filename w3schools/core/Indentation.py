37655e924a3d8617ea484aa2be3dd31e4a341d3e
Noman Ahmed <NomanAhmed@macbookpro.mynetworksettings.com>
2025-02-01 14:56:19 -0500

    updates

---
+++ w3schools/core/Indentation.py
@@ -0,0 +1,18 @@
+# Correct
+if 5 > 2:
+  print("Five is greater than two!")
+
+if 5 > 2:
+ print("Five is greater than two!")
+if 5 > 2:
+        print("Five is greater than two!")
+
+# Incorrect because there is no indentation after if condition
+# if 5 > 2:
+# print("Five is greater than two!")
+
+# Incorrect because there are two different indentation after if condition
+# if 5 > 2:
+#  print("Five is greater than two!")
+#         print("Five is greater than two!")
+
