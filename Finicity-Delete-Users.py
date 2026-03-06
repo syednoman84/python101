cc81c40f942f9d067ba0f69dea6e634282e2e605
NomanAhmed <noman.ahmed@amount.com>
2026-03-06 12:26:17 -0500

    Updates

--- Finicity-Delete-Users.py
+++ Finicity-Delete-Users.py
@@ -3,8 +3,8 @@ import requests
 if __name__ == '__main__':
     search_url = 'https://api.finicity.com/aggregation/v1/customers'
     headers = {
-        'Finicity-App-Key': 'eb9db7587f4bcc2aebfcf43f6515b3fb',
-        'Finicity-App-Token': 'lbS6aV5KB7CI0eiUIS9q',
+        'Finicity-App-Key': 'XXX',
+        'Finicity-App-Token': 'XXX',
         'Accept': 'application/json'
     }
     payload = {'start': 1, 'limit': 25}
